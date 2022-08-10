ce1e066ccd5df6e0928a80c30a029b2cf1f1e654
Omjannu <89771105+Om-jannu@users.noreply.github.com>
2022-08-10 07:12:06 -0700

    made basic layout

--- src/App.js
+++ src/App.js
@@ -81,7 +81,7 @@ const ChatMessage =(props) => {
     const messageClass = uid === auth.currentUser.uid ? "sent" : "received";
   return (
     <div className={`message ${messageClass}`}>
-      <img src={photoURL}/>
+      <img alt={auth.currentUser.displayName} src={photoURL}/>
       <p>{text}</p>
     </div>
   )
